7f64330adf127b688b55441fcae5e5f27bfcfeaa
Ifrah789 <131014759+Ifrah789@users.noreply.github.com>
2023-04-18 17:56:41 +0200

    Add files via upload

---
+++ Programes/ProgrameTest/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+
+namespace ProgramTest
+{
+    [TestFixture]
+    public class ProcessMonitorTest
+    {
+        [Test]
+        public void Monitor_Calculator_Less_Than_Max_Lifetime_Does_Not_Kill_It()
+        {
+            const string calculatorPath = "/System/Applications/Calculator.app";
+            const string processName = "Calculator";
+            const Double maxLifetime = 0.30;
+            const int frequency = 1;
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = calculatorPath,
+                UseShellExecute = true
+            };
+            var process = Process.Start(startInfo);
+
+            var thread = new Thread(() => Program.Main(new[] { processName, maxLifetime.ToString(), frequency.ToString() }));
+            thread.Start();
+            Thread.Sleep(5000);
+
+            process.Refresh();
+            Assert.That(process.HasExited, Is.False);
+
+            process.CloseMainWindow();
+            thread.Join();
+        }
+    }
+}
---
+++ Programes/Programes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ProcessMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Please enter all three argunments <process name> <max lifetime in minutes> <monitoring frequency in minutes>");
+                return;
+            }
+
+            string processName = args[0];
+            int maxProcessLifetime = int.Parse(args[1]);
+            int frequencyMonitor = int.Parse(args[2]);
+
+            bool exitFlag = false;
+            do
+            {
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Q)
+                {
+                    exitFlag = true;
+                    Console.WriteLine("Quitting...");
+                }
+                else
+                {
+                    var processes = Process.GetProcessesByName(processName);
+                    foreach (var process in processes)
+                    {
+                        var lifetime = DateTime.Now - process.StartTime;
+                        if (lifetime.TotalMinutes > maxProcessLifetime)
+                        {
+                            Console.WriteLine($"Killing process {process.ProcessName}, started at {process.StartTime}, lifetime {lifetime.TotalMinutes:F2} minutes");
+                            process.Kill();
+                        }
+                    }
+
+                    System.Threading.Thread.Sleep(frequencyMonitor * 60 * 1000);
+                }
+            } while (!exitFlag);
+        }
+    }
+}
